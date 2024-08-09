2bb96484ed5016f7080077acd786feb085e46e12
marguellocasas <114915286+marguellocasas@users.noreply.github.com>
2024-08-09 20:42:13 +0200

    Update ejercicio11.py

---
+++ ejercicio11.py
@@ -0,0 +1,12 @@
+anionacimiento = float(input("ingrese año de nacimiento"))
+
+def calculoedad(anio):
+
+  anio_actual = 2024
+  edad = anio_actual - anio
+
+
+
+  return edad
+
+print(f"tu edad es '{calculoedad(anionacimiento)}'")
