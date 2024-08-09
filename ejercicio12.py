6562844725dc2e22b2dd2c90a54cdc363318ee41
marguellocasas <114915286+marguellocasas@users.noreply.github.com>
2024-08-09 21:17:04 +0200

    Create ejercicio12.py

---
+++ ejercicio12.py
@@ -0,0 +1,7 @@
+variable1 = float(input("ingrese longitud del rectangulo"))
+variable2 = float(input("ingrese ancho del rectangulo"))
+
+def arearectangulo(longitud, ancho):
+  return longitud * ancho
+
+print(f" el area del rectangulo es  '{arearectangulo(variable1,variable2)}'")
