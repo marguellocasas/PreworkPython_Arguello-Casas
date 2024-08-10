069fbdcb27c18cd162a6e5a5b01a414dd0439cdd
marguellocasas <114915286+marguellocasas@users.noreply.github.com>
2024-08-10 18:18:15 +0200

    ejercicios

---
+++ ejercicio14.py
@@ -0,0 +1,6 @@
+precio = float(input("ingrese monto total"))
+
+def calculedescuento(descuento):
+  return precio * 0.20
+
+print(f"el monto total con descuento es de: '{calculedescuento(precio)}'")
